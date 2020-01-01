79b038b6418e71f25a6027de51f8eeafe99dcf01
Carlos Nunez <carlosonunez@noreply.github.com>
2020-01-01 16:11:53 -0600

    Support onReceive, onFinish and templates

--- expensify/api.ts
+++ expensify/api.ts
@@ -1,13 +1,16 @@
-function runExpensifyFunction(jobJson, jobType = "get") {
+function runExpensifyFunction(jobJson, jobType = "get", outputSettings = {},
+  onReceive = false, onFinish = {}, template = "") {
   var userId = getProperty("expensify_api_username")
   var secret = getProperty("expensify_api_secret_key")
   var url = getProperty("expensify_integrations_url")
   var jobDescription = generateRequestJobDescription(jobType,
-    userId, secret, jobJson)
-  var options = {
-    'method': 'POST',
-    'payload': "requestJobDescription="+JSON.stringify(jobDescription)
+    userId, secret, jobJson, outputSettings, onReceive, onFinish)
+  var payload = `requestJobDescription=${JSON.stringify(jobDescription)}`
+  if (template != "") {
+    payload = `${payload}&template=${encodeURI(template)}`
   }
+  var options = { 'method': 'POST','payload': payload }
+  console.log("POST to " + url + ", options: " + JSON.stringify(options))
   var response = UrlFetchApp.fetch(url, options)
   if (response.getResponseCode() != 200) {
     return showError("Expensify API call failed: " + response.getContentText())
@@ -16,7 +19,7 @@ function runExpensifyFunction(jobJson, jobType = "get") {
 }
 
 function generateRequestJobDescription(jobType, userId, secret,
-  input = {}, output = {}, onReceive = {}, onFinish = {}) {
+  input = {}, output = {}, onReceive = false, onFinish = {}) {
   var jobDescription = {
     "type": jobType,
     "credentials": {
@@ -25,9 +28,11 @@ function generateRequestJobDescription(jobType, userId, secret,
     },
     "inputSettings": input,
     "outputSettings": output,
-    "onReceive": onReceive,
     "onFinish": onFinish
   };
+  if (onReceive) {
+    jobDescription.onReceive = { "immediateResponse":["returnRandomFileName"] }
+  }
   return jobDescription;
 }
 
